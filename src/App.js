512892f325fc7a4228217ccc7c426ea8d5ee3ede
vitaly.co@gmail.com <vitaly.co@gmail.com>
2020-12-31 18:38:46 +0300

    - Added component TodoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from './Todo/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+	render() {
+		const todos = [
+			{id: 1, completed: false, title: 'Buy bread'},
+			{id: 2, completed: false, title: 'Buy milk'},
+			{id: 3, completed: false, title: 'Buy beef'},
+		];
+		return (
+			<div className="wrapper">
+				<h1>Hello</h1>
+
+				<TodoList todos={todos} />
+				
+			</div>
+		);
+	}
 }
 
 export default App;

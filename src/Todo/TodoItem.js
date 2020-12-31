2081bf7bea57f13a315b1e3fb9b884fc754b4a47
vitaly.co@gmail.com <vitaly.co@gmail.com>
2020-12-31 19:25:05 +0300

    - Added css styles for TodoList and TodoItem

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
+
 class TodoItem extends React.Component {
 
-	
 	render() {
+		const styles = {
+			li: {
+				display: 'flex',
+				justifyContent: 'space-between',
+				alignItems: 'center',
+				padding: '.5rem 1rem',
+				border: '1px solid #ccc',
+				borderRadius: '4px',
+				marginBottom: '.5rem',
+			},
+			input: {
+				marginRight: '1rem',
+			},
+		};
+
 		return (
-			<li>
-				<strong>{this.props.index + 1}</strong>
-				{this.props.todo.title}
+			<li style={styles.li}>
+				<span>
+					<input style={styles.input} type="checkbox"/>
+					<strong>{this.props.index + 1}</strong>
+					&nbsp;{this.props.todo.title}
+				</span>
+				<button className="rm">&times;</button>
 			</li>
 		);
 	}

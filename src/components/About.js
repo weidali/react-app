44158eb6fd0421efd5628e080fcc6b88b6e4a3f5
vitaly.co@gmail.com <vitaly.co@gmail.com>
2021-01-03 12:51:20 +0300

    - About

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,12 @@ import React from 'react';
 class About extends React.Component {	
 	render() {
 		return (
-			<p>About page</p>
+			<div class="jumbotron">
+				<div class="container">
+					<h1 class="display-4">About page</h1>
+					<p class="lead">React aplication ToDo version 1.0.1</p>
+				</div>
+			</div>
 		);
 	}
 }
